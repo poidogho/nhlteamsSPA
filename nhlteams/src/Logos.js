a3704bfa83ac64a62fe7852211d3a03be86b9c8c
poidogho <poidogho@yahoo.com>
2020-11-15 13:33:29 -0500

    more logos

--- nhlteams/src/Logos.js
+++ nhlteams/src/Logos.js
@@ -25,8 +25,52 @@ const Logos = ({ logo }) => {
     src = "/assets/img/tor.png";
   } else if (logo.toLowerCase().trim() === "car") {
     src = "/assets/img/car.png";
+  } else if (logo.toLowerCase().trim() === "fla") {
+    src = "/assets/img/fla.png";
+  } else if (logo.toLowerCase().trim() === "tbl") {
+    src = "/assets/img/tbl.png";
+  } else if (logo.toLowerCase().trim() === "wsh") {
+    src = "/assets/img/wsh.png";
+  } else if (logo.toLowerCase().trim() === "chi") {
+    src = "/assets/img/chi.png";
+  } else if (logo.toLowerCase().trim() === "det") {
+    src = "/assets/img/det.png";
+  } else if (logo.toLowerCase().trim() === "nsh") {
+    src = "/assets/img/nsh.png";
+  } else if (logo.toLowerCase().trim() === "stl") {
+    src = "/assets/img/stl.png";
+  } else if (logo.toLowerCase().trim() === "cgy") {
+    src = "/assets/img/cgy.png";
+  } else if (logo.toLowerCase().trim() === "col") {
+    src = "/assets/img/col.png";
+  } else if (logo.toLowerCase().trim() === "edm") {
+    src = "/assets/img/edm.png";
+  } else if (logo.toLowerCase().trim() === "van") {
+    src = "/assets/img/van.png";
+  } else if (logo.toLowerCase().trim() === "ana") {
+    src = "/assets/img/ana.png";
+  } else if (logo.toLowerCase().trim() === "ana") {
+    src = "/assets/img/ana.png";
+  } else if (logo.toLowerCase().trim() === "dal") {
+    src = "/assets/img/dal.png";
+  } else if (logo.toLowerCase().trim() === "lak") {
+    src = "/assets/img/lak.png";
+  } else if (logo.toLowerCase().trim() === "sjs") {
+    src = "/assets/img/sjs.png";
+  } else if (logo.toLowerCase().trim() === "cbj") {
+    src = "/assets/img/cbj.png";
+  } else if (logo.toLowerCase().trim() === "min") {
+    src = "/assets/img/min.png";
+  } else if (logo.toLowerCase().trim() === "wpg") {
+    src = "/assets/img/wpg.png";
+  } else if (logo.toLowerCase().trim() === "ari") {
+    src = "/assets/img/ari.png";
+  } else if (logo.toLowerCase().trim() === "ari") {
+    src = "/assets/img/ari.png";
+  } else if (logo.toLowerCase().trim() === "vgk") {
+    src = "/assets/img/vgk.png";
   }
-  console.log(src);
+
   return (
     <img
       src={`${src}`}
@@ -36,6 +80,7 @@ const Logos = ({ logo }) => {
         borderRadius: "20px",
         marginLeft: "3%",
       }}
+      alt="logo"
     />
   );
 };
